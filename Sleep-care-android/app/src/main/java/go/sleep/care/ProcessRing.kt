8a4121f2aebc857040dad296a0aa60768879f823
Jiyoung Hwang <yoi9327@naver.com>
2018-11-19 23:29:09 +0900

    Fix a bug on progressRing component

--- Sleep-care-android/app/src/main/java/go/sleep/care/ProcessRing.kt
+++ Sleep-care-android/app/src/main/java/go/sleep/care/ProcessRing.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class ProcessRing @JvmOverloads constructor(
         context: Context,
@@ -12,8 +13,14 @@ class ProcessRing @JvmOverloads constructor(
         defStyleRes: Int = 0
 ) : LinearLayout(context, attrs, defStyle, defStyleRes) {
 
+    private val progressCircular by lazy {
+        findViewById<CircularProgressBar>(R.id.progress_circular)
+    }
+
     init {
         LayoutInflater.from(context)
                 .inflate(R.layout.gsc_progress_ring, this, true)
+
+        progressCircular.setProgressWithAnimation(65f, 3000)
     }
 }
