f91b09f7b288e6c0165446ae3e9ee6626e99b7f2
Hoseung Lee <astrohsy@gmail.com>
2018-11-29 10:29:06 +0900

    Fix graph issue

--- Sleep-care-android/app/src/main/java/go/sleep/care/ChartFragment.kt
+++ Sleep-care-android/app/src/main/java/go/sleep/care/ChartFragment.kt
@@ -3,6 +3,7 @@ package go.sleep.care
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,23 +17,23 @@ import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class ChartFragment : Fragment() {
+    var myChartView: LineChart? = null;
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view =  inflater.inflate(R.layout.gsc_chart_fragment, container, false)
-        val chart = view.findViewById<LineChart>(R.id.chart)
+        val myChartView = view.findViewById<LineChart>(R.id.chart)
 
 
-        chart.getDescription().setEnabled(false);
-        chart.setTouchEnabled(true);
-        chart.setDrawGridBackground(false)
-        chart.animateX(3000);
+        myChartView.getDescription().setEnabled(false);
+        myChartView.setTouchEnabled(true);
+        myChartView.setDrawGridBackground(false)
 
-        val l = chart.legend
+        val l = myChartView.legend
         l.isEnabled = false
 
-        val xAxis = chart.xAxis
+        val xAxis = myChartView.xAxis
         xAxis.position = XAxis.XAxisPosition.TOP_INSIDE
         xAxis.textSize = 10f
         xAxis.textColor = Color.WHITE
@@ -42,30 +43,30 @@ class ChartFragment : Fragment() {
         xAxis.setCenterAxisLabels(true)
         xAxis.granularity = 1f // one hour
 
-        val leftAxis = chart.axisLeft
+        val leftAxis = myChartView.axisLeft
         leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
         leftAxis.textColor = ColorTemplate.getHoloBlue()
         leftAxis.setDrawGridLines(true)
         leftAxis.isGranularityEnabled = true
-        leftAxis.axisMinimum = 0f
-        leftAxis.axisMaximum = 5000f
+        leftAxis.axisMinimum = -250f
+        leftAxis.axisMaximum = 4100f
         leftAxis.yOffset = -9f
         leftAxis.textColor = Color.rgb(255, 192, 56)
 
-        val rightAxis = chart.axisRight
+        val rightAxis = myChartView.axisRight
         rightAxis.isEnabled = false
 
         var entries = mutableListOf<Entry>()
         entries.add(Entry(6f, 0f))
-        entries.add(Entry(7f, 250f))
-        entries.add(Entry(8f, 1000f))
-        entries.add(Entry(9f, 50f))
-        entries.add(Entry(10f, 30f))
-        entries.add(Entry(11f, 400f))
+        entries.add(Entry(7f, 253f))
+        entries.add(Entry(8f, 1012f))
+        entries.add(Entry(9f, 90f))
+        entries.add(Entry(10f, 63f))
+        entries.add(Entry(11f, 412f))
         entries.add(Entry(12f, 2f))
         entries.add(Entry(13f, 3653f))
-        entries.add(Entry(14f, 1500f))
-        entries.add(Entry(15f, 100f))
+        entries.add(Entry(14f, 1521f))
+        entries.add(Entry(15f, 106f))
         entries.add(Entry(16f, 534f))
 
 
@@ -82,10 +83,17 @@ class ChartFragment : Fragment() {
         dataSet.axisDependency = YAxis.AxisDependency.LEFT
 
         var lineData = LineData(dataSet)
-        chart.data = lineData
-        chart.invalidate()
+        myChartView.data = lineData
+        myChartView.invalidate()
+
+        myChartView?.animateX(1500);
 
 
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("onResume", "hey")
     }
 }
